0459d559018f903c620b40899018e9ba6a95d39f
amarachi-nwokocha <nwokichaamarachi@gmail.com>
2025-08-31 23:01:36 +0100

    edited supabase to superbase

--- app/api/votes/stats/route.ts
+++ app/api/votes/stats/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server"
-import { supabase } from "../../../lib/supabaseServer"
+import { supabase } from "../../../lib/superbaseServer"
 
 export async function GET() {
   try {
