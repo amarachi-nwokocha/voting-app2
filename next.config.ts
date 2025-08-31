15f2d26ec06e83f50bfd8b5a0e22710d8f3b8e6c
amarachi-nwokocha <nwokichaamarachi@gmail.com>
2025-08-31 18:33:59 +0100

    making hub work

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/7for7', // 👈 this is the key fix
+  basePath: "/7for7",
+
+  async redirects() {
+    return [
+      {
+        source: "/7for7",
+        destination: "/7for7/",
+        permanent: true,
+      },
+    ];
+  },
 
   images: {
     remotePatterns: [
