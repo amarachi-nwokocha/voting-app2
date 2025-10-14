e4a8ad7919548bac18d7ef559107a21d0b499efe
amarachi-nwokocha <nwokichaamarachi@gmail.com>
2025-10-14 08:31:40 +0100

    4days

--- app/components/ui/Countdown.tsx
+++ app/components/ui/Countdown.tsx
@@ -24,7 +24,7 @@ export default function CountdownTimer() {
     const getTargetDate = () => {
       const now = new Date();
       // add 4 days in milliseconds
-      return new Date(now.getTime() + 5 * 24 * 60 * 60 * 1000);
+      return new Date(now.getTime() + 4 * 24 * 60 * 60 * 1000);
     };
 
     const targetDate = getTargetDate();
